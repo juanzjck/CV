386e1c0a339d9ceab7c37d5b64c5fc9cbaa8527a
Juan Salazar <juanzjck1996@gmail.com>
2020-04-01 20:32:58 -0500

    Git hub

--- cv/src/component/Slider.js
+++ cv/src/component/Slider.js
@@ -1,14 +1,58 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import perfil  from "../images/yo.jpg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+const fetch = require('node-fetch');
 class Slider extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {  
+            loading:true,
+            error:null,
+            data:[]
+        }
     }
-    render() { 
-        return ( <div className="slider">
+    componentDidMount (){
+        
+        this.fetchData();
+    }
+    fetchData=async ()=>{
+            this.setState({loading:true,error:null});
+            try{
+             
+                const result = await fetch('https://api.github.com/repos/juanzjck/CV/commits', { method: 'GET'});
+                const data= await result.json();
+                this.setState({loading:false,data:data });
+                console.log(data);
+             
+            }catch(e){
+                this.setState({loading:false, error:'Ocurrio un error'});
+                console.log(e);
+            }
+            
+    }
+    isLoading=()=>{
+            return this.state.loading===true? '...': this.state.data.map((doc)=>{
+                return (
+                    <div className="git__item">
+                        <p className="git--title">{doc.commit.message}</p>
+                        <p>
+                            {doc.commit.committer.name}
+                        </p>
+                        <p>
+                            {doc.commit.committer.date}
+                        </p>
+                    </div >
+                )
+            }
+        );
+    }
+
+    render() {
+       
+   
+        return ( 
+        <div className="slider">
                         <div className="slider__profile">
                                 <img className="slider__image" src={perfil}></img>
                         </div>
@@ -21,6 +65,10 @@ class Slider extends Component {
                                 <p className="phrase">
                                         "Never giving up, just make it"
                                 </p>
+                                <div className="git">
+                                    {this.isLoading()}
+                                 
+                                </div>
                             </div>
                            
                             <div className="social">
